7cdf737e8c8e861b5d8b712d39ab247753677b05
shriharipb <123650545+shriharipb@users.noreply.github.com>
2023-01-28 12:36:05 +0530

    shri

--- Sample/src/tree/FirstProgram.java
+++ Sample/src/tree/FirstProgram.java
@@ -5,6 +5,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub");
+			
 }
 
 }
---
+++ Sample/src/tree/Login.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Login {
+
+public static void main(String[] args) {
+		
+		System.out.println("Hello GitHub");
+		System.out.println("Hi GitHub");
+		
+}
+
+
+}
